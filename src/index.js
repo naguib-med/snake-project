1375e26b575e23bf486e4718c23bde6eba6987a6
naguib-med <naguib.mohamed1999@gmail.com>
2022-08-10 12:19:43 +0200

    changement de direction et collisions de défaite

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import "./style.css";
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const direction = "s";
+const speed = 800;
+let direction = "s";
 const gridElem = 40;
 const apple = [5, 5];
 
@@ -30,6 +31,43 @@ const drawApple = () => {
   ctx.fillRect(apple[0] * gridElem, apple[1] * gridElem, gridElem, gridElem);
 };
 
+window.addEventListener("keydown", (event) => {
+  const key = event.key;
+  switch (key) {
+    case "ArrowUp":
+      direction = "n";
+      break;
+    case "ArrowDown":
+      direction = "s";
+      break;
+    case "ArrowRight":
+      direction = "e";
+      break;
+    case "ArrowLeft":
+      direction = "o";
+      break;
+  }
+});
+
+const gameOver = () => {
+  if (
+    snake[0][0] > 19 ||
+    snake[0][0] < 0 ||
+    snake[0][1] > 19 ||
+    snake[0][1] < 0
+  ) {
+    return true;
+  } else {
+    const [head, ...body] = snake;
+    for (let bodyElement of body) {
+      if (bodyElement[0] === head[0] && bodyElement[1] === head[1]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
 const updateSnakePosition = () => {
   let head;
   switch (direction) {
@@ -51,16 +89,20 @@ const updateSnakePosition = () => {
 
   snake.unshift(head);
   snake.pop();
+  return gameOver();
 };
 
 const move = () => {
-  updateSnakePosition();
-  drawMap();
-  drawSnake();
-  drawApple();
-  setTimeout(() => {
-    requestAnimationFrame(move);
-  }, 1000);
+  if (!updateSnakePosition()) {
+    drawMap();
+    drawSnake();
+    drawApple();
+    setTimeout(() => {
+      requestAnimationFrame(move);
+    }, 1000 - speed);
+  } else {
+    alert("perdu");
+  }
 };
 
 requestAnimationFrame(move);
